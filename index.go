a8b398effdaf6e1fed947b7a91e6ad9d58ed8ba7
Adrian Barabino <adrian.barabino@gmail.com>
2023-12-11 03:11:12 +0000

    Fixed

--- index.go
+++ index.go
@@ -3,6 +3,7 @@ package main
 import (
     "crypto/sha256"
     "database/sql"
+	"errors"
     "encoding/base64"
     "log"
     "net/http"
@@ -19,13 +20,32 @@ type AcortarRequest struct {
     URL string `json:"url"`
 }
 
-func generarClaveCorta(url string) string {
+func contarURLs(db *sql.DB) (int, error) {
+    var count int
+    row := db.QueryRow("SELECT COUNT(*) FROM urls")
+    err := row.Scan(&count)
+    return count, err
+}
+
+func generarClaveCorta(db *sql.DB, url string) (string, error) {
+    count, err := contarURLs(db)
+    if err != nil {
+        return "", err
+    }
+
+    longitudClave := 4
+    if count > 10000 { // Umbral para cambiar la longitud de la clave
+        longitudClave = 6
+    }
+
     hasher := sha256.New()
     hasher.Write([]byte(url))
     hash := hasher.Sum(nil)
-    base64Encoded := base64.StdEncoding.EncodeToString(hash)[:10]
-    return base64ToBase62(base64Encoded)
+    base64Encoded := base64.StdEncoding.EncodeToString(hash)[:longitudClave]
+    return base64ToBase62(base64Encoded), nil
 }
+
+
 
 func base64ToBase62(s string) string {
     var result string
@@ -54,7 +74,16 @@ func initDB() *sql.DB {
     return db
 }
 
+func claveCortaExiste(db *sql.DB, claveCorta string) bool {
+    var id int
+    err := db.QueryRow("SELECT id FROM urls WHERE urlAcortada = ?", claveCorta).Scan(&id)
+    return err == nil
+}
+
 func insertURL(db *sql.DB, urlOriginal, urlAcortada string) error {
+    if claveCortaExiste(db, urlAcortada) {
+        return errors.New("la clave corta ya existe")
+    }
     statement, err := db.Prepare("INSERT INTO urls (urlOriginal, urlAcortada) VALUES (?, ?)")
     if err != nil {
         return err
@@ -62,6 +91,7 @@ func insertURL(db *sql.DB, urlOriginal, urlAcortada string) error {
     _, err = statement.Exec(urlOriginal, urlAcortada)
     return err
 }
+
 
 func getOriginalURL(db *sql.DB, urlAcortada string) (string, error) {
     var urlOriginal string
@@ -73,13 +103,22 @@ func getOriginalURL(db *sql.DB, urlAcortada string) (string, error) {
 func main() {
     puerto := os.Getenv("PORT")
     if puerto == "" {
-        puerto = "8085"
+        puerto = "8099"
     }
 
     db := initDB()
     defer db.Close()
 
     router := gin.Default()
+
+	router.Use(func(c *gin.Context) {
+		if c.GetHeader("X-Forwarded-Proto") == "http" {
+			c.Redirect(http.StatusMovedPermanently, "https://"+c.Request.Host+c.Request.URL.RequestURI())
+			return
+		}
+		c.Next()
+	})
+	
 
     // Sirve archivos estáticos desde el directorio 'public'
     router.Static("/static", "./public")
@@ -89,22 +128,37 @@ func main() {
         c.File("./public/index.html")
     })
 
-    router.POST("/acortar", func(c *gin.Context) {
-        var req AcortarRequest
-        if err := c.BindJSON(&req); err != nil {
-            c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-            return
-        }
-
-        claveCorta := generarClaveCorta(req.URL)
-        err := insertURL(db, req.URL, claveCorta)
-        if err != nil {
-            c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al guardar en la base de datos"})
-            return
-        }
-
-        c.JSON(http.StatusOK, gin.H{"urlAcortada": claveCorta})
-    })
+	router.POST("/acortar", func(c *gin.Context) {
+		var req AcortarRequest
+		if err := c.BindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+	
+		var claveCorta string
+		var err error
+		for intentos := 0; intentos < 10; intentos++ {
+			claveCorta, err = generarClaveCorta(db, req.URL)
+			if err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Error al generar la clave corta"})
+				return
+			}
+	
+			err = insertURL(db, req.URL, claveCorta)
+			if err == nil {
+				break
+			}
+		}
+	
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "No se pudo generar una clave corta única"})
+			return
+		}
+	
+		c.JSON(http.StatusOK, gin.H{"urlAcortada": claveCorta})
+	})
+	
+	
 
     router.GET("/:claveCorta", func(c *gin.Context) {
         claveCorta := c.Param("claveCorta")
@@ -122,5 +176,9 @@ func main() {
         c.Redirect(http.StatusMovedPermanently, urlOriginal)
     })
 
+
+
+	// imprimir el puerto en el que se está ejecutando
+	log.Println("Escuchando en el puerto " + puerto)
     router.Run(":" + puerto)
 }
